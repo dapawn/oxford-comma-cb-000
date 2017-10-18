c681e766655f809b342f0a88db3aa117a04f5cb2
David Kuruvilla <dapawn@gmail.com>
2017-10-18 18:57:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
-  array.length > 1 ? array[array.length-1] = array[array.length-1].prepend("and ") :
+  array.length > 1 ? array[array.length-1] = array[array.length-1].prepend("and ") : ;
   array.length > 2 ? array.join(", ") : array.join(" ")
 end
